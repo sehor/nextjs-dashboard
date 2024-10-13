2d32a0c795fad2104e0885768ae33f2b965fb913
sehor <714534524@qq.com>
2024-10-13 16:19:39 +0800

    ppr

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -13,25 +14,21 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function CardWrapper({
-  numberOfInvoices,
-  numberOfCustomers,
-  totalPaidInvoices,
-  totalPendingInvoices,
-}: {
-  numberOfInvoices: number;
-  numberOfCustomers: number;
-  totalPaidInvoices: string;
-  totalPendingInvoices: string;
-}) {
+export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
      return (
       <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
+      <Card key="collected" title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card key="pending" title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card key="invoices" title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card key="customers"
         title="Total Customers"
         value={numberOfCustomers}
           type="customers"
